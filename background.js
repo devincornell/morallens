10bd01c278bbb1730b009ba331f4618ef6bfcf41
Devin Cornell <devinj.cornell@gmail.com>
2019-02-10 12:58:47 -0800

    Set up for parsing from local file. Need to figure out where to find on web.

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
+	chrome.storage.local.set({color: '#3aa757'}, function() {
 		console.log("The color is green.");
 	});
+
+	var dict = getMFD2()
+    chrome.storage.local.set({'dictionary': dict}, function() {
+		//console.log('dict is set to ' + dict);
+		console.log(dict)
+    });
 });
 
 
--- content.js
+++ content.js
@@ -1,82 +1,4 @@
 
-
-function convertRegex(str) {
-    var nstr = str;
-    nstr = nstr.replace('*','\\w*');
-    nstr = nstr.replace(/\s+/,'\\ ');
-    return nstr;
-}
-
-function parsedicttext(dtext) {
-    lines = dtext.split('\n');
-    var catcodemap = {};
-    var wordcats = [];
-    var dict = {}; // object version of wordcats
-    var usedwords = [];
-    var defmode = false;
-    for (var i = 0; i < lines.length; i++) {
-        var tl = lines[i].trim().split(/\s+/i);
-        tl = tl.filter(function (w) { return w != '' })
-
-        if (tl.length > 0 && tl[0] == '%') {
-            defmode = !defmode;
-        } else if (tl.length > 0) {
-            if (defmode) {
-                catcodemap[tl[0]] = tl.slice(1,tl.length).join(' ');
-            } else {
-
-                // search from right for first non-code entry
-                var ind = null;
-                for (var j = tl.length-1; j >= 0; j--) {
-                    if (tl[j] in catcodemap) {
-                        ind = j;
-                    }
-                }
-                
-                // give exception if none of the entries match codemap
-                if (!(tl[tl.length-1] in catcodemap) || ind == null) {
-                    console.log(lines[i])
-                    console.log(tl)
-                    throw('dictionary line ' + i.toString() + ' has undefined/no value: ' + lines[i]);
-                }
-
-                // properties of entry
-                var wordkey = convertRegex(tl.slice(0,ind).join(' '));
-                var catkeys = tl.slice(ind,tl.length)
-                var catvalues = [];
-                catkeys.forEach(function(ck) {
-                    catvalues.push(catcodemap[ck]);
-                });
-                //print(wordkey)
-                // Dict entries based on list of word,cats pairs
-                var wordind = usedwords.indexOf(wordkey);
-                if (wordind < 0) {
-                    // make new entry for word with cats
-                    var rawword = tl.slice(0,ind).join(' ');
-
-                    var wordobj = {};
-                    wordobj['word'] = wordkey;
-                    wordobj['rawword'] = rawword;
-                    wordobj['cats'] = catvalues;
-                    usedwords.push(wordkey);
-                    wordcats.push(wordobj);
-                } else {
-                    // entry already exists, just add catss
-                    wordcats[wordind]['cats'] = wordcats[wordind]['cats'].concat(catvalues);
-                }
-
-                // Dict entries based on object (redundant to wordcats creation)
-                if (!(wordkey in dict)) {
-                    dict[wordkey] = [];
-                }
-                dict[wordkey] = dict[wordkey].concat(catvalues);
-            }
-        }
-    }
-
-    return {'dict':dict, 'codemap':catcodemap, 'wordcats':wordcats, 'usedwords':usedwords};
-}
-
 COLORS = ['6b5b95', 'feb236', 'd64161', 'ff7b25', 'a2b9bc', 'b2ad7f', '878f99', '6b5b95', 'd6cbd3', 'eca1a6', 'bdcebe', 'ada397', 'd5e1df', 'e3eaa7', 'b5e7a0', '86af49']
 
 function assignColors(codemap) {
@@ -91,6 +13,7 @@ function assignColors(codemap) {
 
 var HLTAGTEXT = '<span title="{title}" style="background-color: #{color}">$1</span>';
 function highlightPage(dictobj) {
+    console.log(dictobj);
     var dict = dictobj['dict'];
     var colormap = assignColors(dictobj['codemap']);
 
@@ -114,26 +37,11 @@ function highlightPage(dictobj) {
 
 }
 
+chrome.storage.local.get('dictionary', function(dict) {
+    highlightPage(dict['dictionary']);
+});
 
 
-
-// for parsing dict (later should be in "on install" event handler)
-mfdurl = 'https://www.moralfoundations.org/sites/default/files/files/downloads/moral%20foundations%20dictionary.dic'
-var xhr = new XMLHttpRequest();
-xhr.open("GET", mfdurl, true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    //var resp = JSON.parse(xhr.responseText);
-    var dict = parsedicttext(xhr.responseText);
-    chrome.storage.local.set({'dictionary': dict}, function() {
-      //console.log('dict is set to ' + dict);
-      console.log(dict)
-      highlightPage(dict)
-    });
-  }
-}
-xhr.send();
 
 
 
